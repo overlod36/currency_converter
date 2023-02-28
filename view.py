276a4cfce16aef585893c1797cb1794993c28f13
kirbi <Biehan@mail.ru>
2023-02-28 12:45:45 +0300

    choice logic

--- view.py
+++ view.py
@@ -1,4 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QPushButton, QWidget, QMainWindow, QStyle, QVBoxLayout, QComboBox, QLineEdit
+from PyQt6.QtWidgets import (QApplication, QPushButton, QWidget, 
+                             QMainWindow, QStyle, QVBoxLayout, QComboBox, QLineEdit)
 from PyQt6.QtCore import QSize, Qt
 
 class MainWindow(QMainWindow):
@@ -18,7 +19,11 @@ class MainWindow(QMainWindow):
         self.get_button = QPushButton("Обновить")
         self.about_button = QPushButton("Справка")
 
+        
         self.currency_from = QComboBox()
+        self.currency_from.addItems(['1','2','3'])
+        self.currency_from.currentTextChanged.connect( self.cf_choice )
+        self.currency_from_choice = self.currency_from.currentText()
         self.currency_to = QComboBox()
         self.input_from = QLineEdit()
         self.input_to = QLineEdit()
@@ -32,15 +37,18 @@ class MainWindow(QMainWindow):
         self.main_layout.addWidget(self.get_button)
         self.main_layout.addWidget(self.about_button)
 
-
         self.widget = QWidget()
         self.widget.setLayout(self.main_layout)
 
         self.setCentralWidget(self.widget)
         self.show()
     
+    def cf_choice(self, s):
+        self.currency_from_choice = s
+
     def button_clicked(self):
-        self.button.setText("You already clicked me.")
+        print(self.currency_from_choice)
+
 
 app = QApplication([])
 
