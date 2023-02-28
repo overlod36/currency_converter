27208d51ba9271e8b7a279e8bb3155c0561a1da4
kirbi <Biehan@mail.ru>
2023-02-28 12:12:40 +0300

    addin widgets

--- view.py
+++ view.py
@@ -1,26 +1,49 @@
-from PyQt6.QtWidgets import QApplication, QPushButton, QWidget, QMainWindow
+from PyQt6.QtWidgets import QApplication, QPushButton, QWidget, QMainWindow, QStyle, QVBoxLayout, QComboBox, QLineEdit
 from PyQt6.QtCore import QSize, Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        self.initialize()
+        
+    def initialize(self):
         self.setWindowTitle("Конвертор валют")
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.button_clicked)
-
+        self.setWindowIcon(self.style().standardIcon(QStyle.StandardPixmap.SP_DirHomeIcon))
         self.setFixedSize(QSize(400, 300))
 
-        # Устанавливаем центральный виджет Window.
-        self.setCentralWidget(button)
+        self.conv_button = QPushButton("Конвертация")
+        self.conv_button.setCheckable(True)
+        self.conv_button.clicked.connect(self.button_clicked)
+        
+        self.get_button = QPushButton("Обновить")
+        self.about_button = QPushButton("Справка")
+
+        self.currency_from = QComboBox()
+        self.currency_to = QComboBox()
+        self.input_from = QLineEdit()
+        self.input_to = QLineEdit()
+
+        self.main_layout= QVBoxLayout()
+        self.main_layout.addWidget(self.currency_from)
+        self.main_layout.addWidget(self.input_from)
+        self.main_layout.addWidget(self.currency_to)
+        self.main_layout.addWidget(self.input_to)
+        self.main_layout.addWidget(self.conv_button)
+        self.main_layout.addWidget(self.get_button)
+        self.main_layout.addWidget(self.about_button)
+
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.main_layout)
+
+        self.setCentralWidget(self.widget)
+        self.show()
     
     def button_clicked(self):
-        print(1)
+        self.button.setText("You already clicked me.")
 
 app = QApplication([])
 
 window = MainWindow()
-window.show()
 
 app.exec()
